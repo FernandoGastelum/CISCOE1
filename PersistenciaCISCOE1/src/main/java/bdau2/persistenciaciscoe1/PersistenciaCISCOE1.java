4b1767d8216356b8a8045fa51ae691ee89d3d060
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-04-02 22:58:25 -0700

    Proyecto Persistencia

---
+++ PersistenciaCISCOE1/src/main/java/bdau2/persistenciaciscoe1/PersistenciaCISCOE1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bdau2.persistenciaciscoe1;
+
+/**
+ *
+ * @author gaspa
+ */
+public class PersistenciaCISCOE1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
