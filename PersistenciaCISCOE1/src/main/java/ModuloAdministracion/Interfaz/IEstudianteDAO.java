a26285adf105f41632862cdb71c46c1d434c1068
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-04-02 23:03:18 -0700

    cambio a interfaz

--- PersistenciaCISCOE1/src/main/java/ModuloAdministracion/Interfaz/IEstudianteDAO.java
+++ PersistenciaCISCOE1/src/main/java/ModuloAdministracion/Interfaz/IEstudianteDAO.java
@@ -1,15 +1,25 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package ModuloAdministracion.Interfaz;
 
+import DTOs.EstudianteDTO;
+import DTOs.EstudianteDTOGuardar;
+import Entidades.Estudiante;
+import Excepcion.PersistenciaException;
 import java.util.List;
 
 /**
  *
  * @author gaspa
  */
-public class IEstudianteDAO {
+public interface IEstudianteDAO {
+    Estudiante guardar(EstudianteDTOGuardar alumno) throws PersistenciaException;
     
+    Estudiante obtenerPorID(Long id) throws PersistenciaException;
+    
+    List<Estudiante> obtener() throws PersistenciaException;
+    
+    EstudianteDTO obtenerEstudianteDTO(Long id);
 }
