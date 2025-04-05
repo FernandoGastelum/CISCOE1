662dd920d954af9762137e105cdf897621cb90d9
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-04-04 21:32:35 -0700

    ExcepcionNegocio

---
+++ NegocioCISCOE1/src/main/java/ModuloReservas/Interfaz/IReservaNegocio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ModuloReservas.Interfaz;
+
+import DTOs.ReservaDTO;
+import DTOs.ReservaDTOGuardar;
+import Excepcion.NegocioException;
+import java.util.List;
+
+/**
+ *
+ * @author gaspa
+ */
+public interface IReservaNegocio {
+    ReservaDTO guardar(ReservaDTOGuardar alumno) throws NegocioException;
+    
+    List<ReservaDTO> obtener() throws NegocioException;
+}
